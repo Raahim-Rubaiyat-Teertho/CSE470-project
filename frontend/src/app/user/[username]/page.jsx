c847970c131f9eaa9f441f8fc072f2d0936495a7
Raahim-Rubaiyat-Teertho <raahim.rubaiyat2021@gmail.com>
2024-02-16 23:57:16 +0600

    dashboard api

--- frontend/src/app/user/[username]/page.jsx
+++ frontend/src/app/user/[username]/page.jsx
@@ -1,33 +1,54 @@
-// 'use client'
+'use client'
 import { getSessionData } from "@/app/login/actions"
 import { getSessionId } from "@/app/login/handleSessions"
-// import { useEffect } from "react"
+import { Button } from "@/components/ui/button"
+import { useState, useEffect } from 'react'
 
-export default async function UserDashboard({params}) {
-    const sess = await getSessionId();
-    console.log(sess);
+export default function UserDashboard({params}) {
+    const sess = params.username
+    const [data, setData] = useState("")
+    
+    async function getInfo () {
+      const lnk= `http://localhost:8000/users/uname/${sess}`;
+      const lnk_f = await fetch(lnk);
+      const lnk_j = await lnk_f.json();
+      setData(lnk_j)
+      console.log(lnk_j)
+    }
+
+    useEffect(
+      () => {
+        getInfo();
+      }, []
+    );
+
     return(
         <>
             <title>Dashboard</title>
             <div className=" min-h-screen py-8">
       <div className="max-w-lg mx-auto bg-white shadow-md rounded-md p-8">
-        <h1 className="text-3xl font-semibold mb-4 text-center">Welcome, hbhjvgh</h1>
+        <h1 className="text-3xl font-semibold mb-4 text-center">Welcome, {data.name}</h1>
         <div className="grid grid-cols-2 gap-4">
           <div>
             <p className="font-medium">Username:</p>
-            <p>asdgag</p>
+            <p>{data.uname}</p>
           </div>
           <div>
             <p className="font-medium">Email:</p>
-            <p>hsrsghsr</p>
+            <p>{data.email}</p>
           </div>
           <div>
             <p className="font-medium">Date of Birth:</p>
-            <p>gsdggd</p>
+            <p>{data.dob}</p>
           </div>
           <div>
             <p className="font-medium">Category:</p>
-            <p>gdgsdgsdgs</p>
+            <p>{data.category}</p>
+          </div>
+
+          <div>
+            <Button>Logout</Button>
+            <Button className="ml-2">Edit Profile</Button>
           </div>
         </div>
       </div>
