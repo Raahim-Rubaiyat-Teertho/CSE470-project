64b829cc833f39b6c0acb00a5015f604baa6d321
Saif <gluttonyenvyfmab@gmail.com>
2024-03-02 00:24:16 +0600

    navnav

--- frontend/src/components/ui/navbar.jsx
+++ frontend/src/components/ui/navbar.jsx
@@ -1,16 +1,25 @@
-export default function Navbar () {
+"use client"
+import { getSessionId } from "@/app/login/handleSessions";
+import Link from "next/link";
+
+export default async function Navbar () {
+    const sess = await getSessionId()
+
     return (
-        <div className="flex justify-between p-4">
+        <div className="flex justify-between p-4 bg-black text-white">
             <div>
                 <p>Listen.Express</p>
             </div>
 
-            <div>
-                links
+            <div className="flex">
+                <div className="pr-5"><Link href="../posts">Posts</Link></div>
+                <div className="pr-5"><Link href="../songs">Songs</Link></div>
+                <div className="pr-5"><Link href="../create">Create</Link></div>
+                <div className="pr-5"><Link href={`../users/${sess}`}></Link></div>
             </div>
 
             <div>
-                logout
+                Logout
             </div>
         </div>
     )
