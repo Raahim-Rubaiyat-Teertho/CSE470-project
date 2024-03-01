c26be2e840a64b2f722c38f065a1392a5ac86953
Raahim-Rubaiyat-Teertho <raahim.rubaiyat2021@gmail.com>
2024-03-01 20:37:24 +0600

    after pay

--- frontend/src/app/paid/[tid]/page.jsx
+++ frontend/src/app/paid/[tid]/page.jsx
@@ -4,7 +4,7 @@ export default function Paid () {
     return (
         <div className="h-screen flex justify-center flex-col items-center">
             <title>Payment Successful!</title>
-            <h1 className="text-4xl font-bold">Payment completed successfully! Go to <Link href='../login' className="h-screen text-green-600">Login Page</Link> </h1>
+            <h1 className="text-4xl font-bold">Payment completed successfully! Go to the <Link href='../login' className="h-screen text-green-600">Login Page</Link> </h1>
         </div>
     )
 }
