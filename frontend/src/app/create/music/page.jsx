daa25049ad9223b8507e5151fc9e24fa3fc5e9a8
Raahim-Rubaiyat-Teertho <raahim.rubaiyat2021@gmail.com>
2024-04-04 01:14:06 +0600

    hm

--- frontend/src/app/create/music/page.jsx
+++ frontend/src/app/create/music/page.jsx
@@ -1,5 +1,14 @@
 'use client'
 
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import { getSessionId } from "@/app/login/handleSessions";
+import { Toaster } from "@/components/ui/sonner";
+import { toast } from "sonner"
+import { useRouter } from 'next/navigation'
+
 import Navbar from "@/components/ui/navbar";
 
 export default function CreateMusic () {
@@ -7,6 +16,15 @@ export default function CreateMusic () {
         <div>
             <title>Create | Music</title>
             <Navbar />
+
+            <h1 className="text-center text-3xl font-semibold mt-7">Post Your Music</h1>
+
+            <form  className="mx-52 ">
+                <Input type='text' placeholder='Title' className='font-bold mt-7 mb-7 placeholder:font-medium' name='title'/>
+                <Input type='file' name='music'/>
+                <Button className='mt-5'>Post</Button>
+            </form>
+            <Toaster />
         </div>
     )
 }
